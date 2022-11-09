bb849c023c1bb31d28d410abd4e012f030da85af
Tokuhiro Matsuno <tokuhirom@gmail.com>
2022-11-09 10:04:17 +0900

    initial import

---
+++ src/main/kotlin/com/example/springbootdemo/SpringbootdemoApplication.kt
@@ -0,0 +1,47 @@
+package com.example.springbootdemo
+
+import kweb.Kweb
+import kweb.h1
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.info.BuildProperties
+import org.springframework.boot.info.GitProperties
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.context.event.EventListener
+import org.springframework.context.support.GenericApplicationContext
+import org.springframework.stereotype.Component
+import java.time.ZoneId
+
+@ConstructorBinding
+@ConfigurationProperties("kweb")
+data class KwebProperties(
+    val port: Int = 8080
+)
+
+@Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(KwebProperties::class)
+class KwebConfiguration {
+    @Bean(destroyMethod = "close")
+    fun kweb(buildProperties: BuildProperties, kwebProperties: KwebProperties): Kweb {
+        return Kweb(port = kwebProperties.port) {
+            doc.body {
+                h1().text(
+                    "Hello World! ${buildProperties.group}:${buildProperties.artifact}:${buildProperties.name}"
+                            + "${buildProperties.version} at ${buildProperties.time.atZone(ZoneId.systemDefault())}"
+                )
+            }
+        }
+    }
+}
+
+@SpringBootApplication
+class SpringbootdemoApplication
+
+fun main(args: Array<String>) {
+    val apps = runApplication<SpringbootdemoApplication>(*args)
+}
